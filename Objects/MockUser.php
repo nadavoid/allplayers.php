b1075c79c8c0beed102d9236afa1f3223a42ab6f
Glenn Pratt <glenn@allplayers.com>
2012-01-04 11:43:55 -0600

    SID-22537291: Replace User interface with abstract class since at this stage we are only interested in fields.

--- Objects/MockUser.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace AllPlayers\Objects;
-
-use AllPlayers\Objects\User;
-
-use stdClass;
-
-class MockUser extends stdClass implements User {}
--- Objects/User.php
+++ Objects/User.php
@@ -1,4 +1,34 @@
 <?php
 namespace AllPlayers\Objects;
 
-interface User {}
+use DateTime;
+
+/**
+ * Defines standard user fields.
+ */
+abstract class User extends stdClass {
+  /**
+   * @var string
+   */
+  public $email;
+
+  /**
+   * @var string
+   */
+  public $firstname;
+
+  /**
+   * @var string
+   */
+  public $lastname;
+
+  /**
+   * @var string
+   */
+  public $gender;
+
+  /**
+   * @var DateTime
+   */
+  public $birthdate;
+}
