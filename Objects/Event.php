0616063b5b892e5bb212ea6ac71d7c81362312dd
Maksim Pecherskiy <max@maksimize.com>
2012-03-06 11:56:27 -0600

    [#26017121] event update and create

---
+++ Objects/Event.php
@@ -0,0 +1,55 @@
+<?php
+namespace AllPlayers\Objects;
+
+use stdClass;
+
+/**
+ * Defines standard Event fields.
+ */
+abstract class Event extends stdClass {
+
+  /**
+   * @var array
+   */
+  public $groups;
+
+  /**
+   * @var string
+   */
+  public $title;
+
+  /**
+   * @var string
+   */
+  public $description;
+
+  /**
+   * @var array
+   */
+  public $date_time;
+
+  /**
+   * @var string
+   */
+  public $category;
+
+  /**
+   * @var array
+   */
+  public $resources;
+
+  /**
+   * @var array
+   */
+  public $competitors;
+
+  /**
+   * @var string
+   */
+  public $published;
+
+  /**
+   * @var string
+   */
+  public $external_id;
+}
