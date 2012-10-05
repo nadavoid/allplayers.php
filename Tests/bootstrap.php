04df2d2e1b1c83c18ce9e34cc244e96565f8d13f
Arturo Contreras <arturo@allplayers.com>
2012-10-05 15:33:42 -0500

    [#36708107] Login and logout endpoint, remove dependent variables.

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -36,15 +36,17 @@ Guzzle\Tests\GuzzleTestCase::setMockBasePath(__DIR__ . DIRECTORY_SEPARATOR . 'mo
 
 require_once 'Objects/User.php';
 require_once 'User/Fixtures/RandomUser.php';
-
+$api_user = (isset($_SERVER['API_USER'])) ? $_SERVER['API_USER'] : NULL;
+$api_password = (isset($_SERVER['API_PASSWORD'])) ? $_SERVER['API_PASSWORD'] : NULL;
+$api_host = (isset($_SERVER['API_HOST'])) ? $_SERVER['API_HOST'] : 'www.pdup.allplayers.com';
 $service_builder = Guzzle\Tests\GuzzleTestCase::setServiceBuilder(Guzzle\Service\Builder\ServiceBuilder::factory(array(
     'admin.basic' => array(
         'auth' => 'basic',
         'class' => 'AllPlayers.AllPlayersClient',
         'params' => array(
-            'username' => $_SERVER['API_USER'],
-            'password' => $_SERVER['API_PASSWORD'],
-            'host' => $_SERVER['API_HOST'],
+            'username' => $api_user,
+            'password' => $api_password,
+            'host' => $api_host,
             'curl.CURLOPT_SSL_VERIFYHOST' => false,
             'curl.CURLOPT_SSL_VERIFYPEER' => false
         )
@@ -53,9 +55,9 @@ $service_builder = Guzzle\Tests\GuzzleTestCase::setServiceBuilder(Guzzle\Service
         'auth' => 'cookies',
         'class' => 'AllPlayers.AllPlayersClient',
         'params' => array(
-            'username' => $_SERVER['API_USER'],
-            'password' => $_SERVER['API_PASSWORD'],
-            'host' => $_SERVER['API_HOST'],
+            'username' => $api_user,
+            'password' => $api_password,
+            'host' => $api_host,
             'curl.CURLOPT_SSL_VERIFYHOST' => false,
             'curl.CURLOPT_SSL_VERIFYPEER' => false
         )
@@ -63,7 +65,7 @@ $service_builder = Guzzle\Tests\GuzzleTestCase::setServiceBuilder(Guzzle\Service
     'anonymous.allplayers' => array(
         'class' => 'AllPlayers.AllPlayersClient',
         'params' => array(
-            'host' => $_SERVER['API_HOST'],
+            'host' => $api_host,
             'curl.CURLOPT_SSL_VERIFYHOST' => false,
             'curl.CURLOPT_SSL_VERIFYPEER' => false
         )
